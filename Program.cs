a97c87659c8a28acc38857ac7a3c562e57f0f981
Ilnur Garrapov <garrapovilnur@gmail.com>
2022-12-13 18:35:14 +0300

    21st ex

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число: ");
+﻿/*
+Console.WriteLine("Введите число: ");
 string number = Console.ReadLine();
 int len = number.Length;
 
@@ -16,4 +17,27 @@ if (len == 5)
 else
 {
     Console.WriteLine($"ОШИБКА: {number} - Не является пятизначным числом");
+}
+*/
+
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
